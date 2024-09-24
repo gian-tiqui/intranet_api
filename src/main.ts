6f69bb54df27abd2da7c57a60123609ae6ac03b6
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-24 09:59:54 +0800

    intranet api

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
