0989ff03355a9d1c9ea0c980687f12949bc40d48
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-01 15:16:32 +0800

    live url added

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const PORT = process.env.PORT || 8080;
+  const liveUrl = process.env.VERCEL_ENV;
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', liveUrl],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders:
       'Origin, Content-Type, Authorization, X-Requested-With, Cache-Control',
