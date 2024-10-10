1e33685077df1ed265a2dd04c57a7459f35d019e
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-10-10 09:10:52 +0800

    added rate limiter to all endpoints and points indicator outside of each class

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -13,6 +13,13 @@ import { DepartmentService } from './department.service';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { JwtAuthGuard } from '../auth/guards/auth.guard';
 import { UpdateDeptDto } from './dto/update-department.dto';
+import { RateLimit } from 'nestjs-rate-limiter';
+
+const FIND_ALL_POINTS = 10;
+const FIND_BY_ID_POINTS = 10;
+const CREATE_POINTS = 5;
+const UPDATE_BY_ID_POINTS = 10;
+const DELETE_BY_ID_POINTS = 10;
 
 @UseGuards(JwtAuthGuard)
 @Controller('department')
@@ -20,30 +27,61 @@ export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   // Departments fetching endpoint
+  @RateLimit({
+    keyPrefix: 'get_departments',
+    points: FIND_ALL_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   @Get()
   findAll(@Query('departmentName') departmenName: string) {
     return this.departmentService.findAll(departmenName);
   }
 
   // One department fetcher endpoint
+
   @Get(':id')
+  @RateLimit({
+    keyPrefix: 'one_department',
+    points: FIND_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   findById(@Param('id') deptId: number) {
     return this.departmentService.findOneById(deptId);
   }
 
   // Department creation endpoint
   @Post()
+  @RateLimit({
+    keyPrefix: 'create_department',
+    points: CREATE_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   create(@Body() createDepartmentDto: CreateDepartmentDto) {
     return this.departmentService.create(createDepartmentDto);
   }
 
   @Put(':id')
+  @RateLimit({
+    keyPrefix: 'update_department_by_id',
+    points: UPDATE_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   updateById(@Param('id') deptId, @Body() updateDeptDto: UpdateDeptDto) {
     return this.departmentService.updateById(deptId, updateDeptDto);
   }
 
   // Department deletion endpoint
   @Delete(':id')
+  @RateLimit({
+    keyPrefix: 'delete_department_by_id',
+    points: DELETE_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   deleteById(@Param('id') deptId: number) {
     return this.departmentService.deleteById(deptId);
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,32 +10,69 @@ import {
 import { UserService } from './user.service';
 import { UpdateUserDTO } from './dto/update-user.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { RateLimit } from 'nestjs-rate-limiter';
+
+const FIND_ALL_POINTS = 10;
+const FIND_BY_ID_POINTS = 10;
+const PASSWORD_POINTS = 5;
+const UPDATE_BY_ID_POINTS = 10;
+const DELETE_BY_ID_POINTS = 10;
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @RateLimit({
+    keyPrefix: 'get_users',
+    points: FIND_ALL_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   getAll() {
     return this.userService.getAll();
   }
 
   @Get(':id')
+  @RateLimit({
+    keyPrefix: 'one_user',
+    points: FIND_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   getById(@Param('id') userId) {
     return this.userService.getById(userId);
   }
 
   @Put(':id')
+  @RateLimit({
+    keyPrefix: 'update_user_by_id',
+    points: UPDATE_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   updateById(@Param('id') userId, @Body() updateUserDto: UpdateUserDTO) {
     return this.userService.updateById(userId, updateUserDto);
   }
 
   @Delete(':id')
+  @RateLimit({
+    keyPrefix: 'delete_user_by_id',
+    points: DELETE_BY_ID_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   deleteById(@Param('id') userId) {
     return this.userService.deleteById(userId);
   }
 
   @Post('password')
+  @RateLimit({
+    keyPrefix: 'change_user_password',
+    points: PASSWORD_POINTS,
+    duration: 60,
+    errorMessage: 'Please wait before posting again.',
+  })
   changePassword(@Body() changePasswordDto: ChangePasswordDto) {
     console.log(changePasswordDto);
     return this.userService.changePassword(
