7840b2a77ad5896ec42412a986e5214c411d80d9
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-11-08 11:50:25 +0800

    added jwt auth to post dept

--- src/post-department/post-department.controller.ts
+++ src/post-department/post-department.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { PostDepartmentService } from './post-department.service';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('post-department')
 export class PostDepartmentController {
   constructor(private readonly postDepartmentService: PostDepartmentService) {}
